052b411f47f118476cc85d5c7ed4559ed20b3a4a
Eric Perez <ericricardoperez@gmail.com>
2014-04-14 10:28:37 -0700

    basic key structure

--- btree.js
+++ btree.js
@@ -1,7 +1,15 @@
 function bTree(isLeaf, isRoot){
   isRoot = isRoot || false
   isLeaf = isLeaf || false
-  
+
   this.isLeaf = isLeaf
   this.isRoot = isRoot
+  this.node = []
 }
+
+function Key(value) {
+  this.value = value
+  this.leftEdge = nil
+  this.rightEdge = nil
+}
+
