6b16f71b7864f09d95823d7339cc0cb1692430af
Eric Perez <ericricardoperez@gmail.com>
2014-04-14 16:01:13 -0700

    linear sorted insert onto leaf node

--- btree.js
+++ btree.js
@@ -9,7 +9,45 @@ function bTree(isLeaf, isRoot){
 
 function Key(value) {
   this.value = value
-  this.leftEdge = nil
-  this.rightEdge = nil
+  this.leftEdge = null
+  this.rightEdge = null
 }
+
+bTree.prototype.insert = function(num){
+  console.log(this)
+  if ( this.isLeaf ) {
+    this.insertIntoLeafNode(num)
+  }
+}
+
+bTree.prototype.insertIntoLeafNode = function(num){
+  var key = new Key(num)
+
+  if (this.node.length === 0){
+    this.node.push(key)
+    return 
+  }
+
+  if (key.value < this.node[0].value){
+    this.node.unshift(key)
+    return
+  }
+
+  if (key.value >= this.node[this.node.length-1].value) {
+    this.node.push(key)
+    return 
+  }
+
+  for (var i = 0; i < this.node.length - 1; i++ ){
+    if (key.value >= this.node[i].value && key.value < this.node[i+1].value){
+      this.node.splice(i+1, 0, key)
+      return
+    }
+  }
+
+}
+
+
+
+
 
