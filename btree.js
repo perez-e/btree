e220fce5a526dabc318effb2355224966eefe782
Eric Perez <ericricardoperez@gmail.com>
2014-04-15 16:04:16 -0700

    handle case when node becomes overflown

--- btree.js
+++ btree.js
@@ -1,96 +1,135 @@
 function bTree(isLeaf, isRoot){
-  isRoot = isRoot || false
-  isLeaf = isLeaf || false
+  isRoot = isRoot || false;
+  isLeaf = isLeaf || false;
 
-  this.isLeaf = isLeaf
-  this.isRoot = isRoot
-  this.node = []
-  this.m = 4
+  this.isLeaf = isLeaf;
+  this.isRoot = isRoot;
+  this.node = [];
+  this.m = 4;
 }
 
 function Key(value) {
-  this.value = value
-  this.leftEdge = null
-  this.rightEdge = null
+  this.value = value;
+  this.leftEdge = null;
+  this.rightEdge = null;
 }
 
 bTree.prototype.insert = function(num){
  
   if ( this.isLeaf ) {
-    this.insertIntoLeafNode(num)
+    this.insertIntoLeafNode(num);
   }
 
   if ( !this.isLeaf ) {
-    this.transverseTree(num)
+    this.transverseTree(num);
   }
 
   if (this.node.length > this.m && this.isRoot){
-    this.newRoot()
+    this.newRoot();
   }
-}
+
+  if (this.node.length > this.m && !this.isRoot ){
+    this.overFlow();
+  }
+
+};
+
+bTree.prototype.overFlow = function(){
+  var mid = Math.floor(this.node.length/2);
+  var key = this.node[mid];
+  var leftNode = this.node.splice(0,mid);
+  var rightNode = this.node.splice(1, this.node.length);
+  var leftTree = new bTree(true);
+  leftTree.upNode = this.upNode;
+  var rightTree = new bTree(true);
+  rightTree.upNode = this.upNode;
+  leftTree.node = leftNode;
+  rightTree.node = rightNode;
+
+  key.leftEdge = leftTree;
+  key.rightEdge = rightTree;
+
+  if (key.value < this.upNode.node[0].value){
+    this.upNode.node.unshift(key);
+    this.upNode.node[1].leftEdge = rightTree;s
+    return;
+  }
+
+  if (key.value >= this.upNode.node[this.upNode.node.length-1].value) {
+    this.upNode.node.push(key);
+    this.upNode.node[this.upNode.node.length - 2].rightEdge = leftTree;
+    return ;
+  }
+
+  for (var i = 0; i < this.upNode.node.length - 1; i++ ){
+    if (key.value >= this.upNode.node[i].value && key.value < this.upNode.node[i+1].value){
+      this.upNode.node.splice(i+1, 0, key);
+      this.upNode.node[i].rightEdge = leftTree;
+      this.upNode.node[i+2].leftEdge = rightTree;
+      return;
+    }
+  }
+  
+
+};
 
 bTree.prototype.transverseTree = function(num){
   if (num < this.node[0].value) {
-    this.node[0].leftEdge.insert(num)
-    return
+    this.node[0].leftEdge.insert(num);
+    return;
   }
 
   if (num >= this.node[this.node.length-1].value) {
-    this.node[this.node.length-1].rightEdge.insert(num)
-    return
+    this.node[this.node.length-1].rightEdge.insert(num);
+    return;
   }
 
   for (var i = 0; i < this.node.length - 1; i++ ){
     if (num >= this.node[i].value && num < this.node[i+1].value) {
-      this.node[i].rightEdge.insert(num)
-      return
+      this.node[i].rightEdge.insert(num);
+      return;
     }
   }
-}
+};
 
 bTree.prototype.insertIntoLeafNode = function(num){
-  var key = new Key(num)
+  var key = new Key(num);
 
   if (this.node.length === 0){
-    this.node.push(key)
-    return 
+    this.node.push(key);
+    return ;
   }
 
   if (key.value < this.node[0].value){
-    this.node.unshift(key)
-    return
+    this.node.unshift(key);
+    return;
   }
 
   if (key.value >= this.node[this.node.length-1].value) {
-    this.node.push(key)
-    return 
+    this.node.push(key);
+    return ;
   }
 
   for (var i = 0; i < this.node.length - 1; i++ ){
     if (key.value >= this.node[i].value && key.value < this.node[i+1].value){
-      this.node.splice(i+1, 0, key)
-      return
+      this.node.splice(i+1, 0, key);
+      return;
     }
   }
 
-}
+};
 
 bTree.prototype.newRoot = function(){
-  var mid = Math.floor(this.node.length/2)
-  var leftNode = this.node.splice(0,mid)
-  var rightNode = this.node.splice(1, this.node.length)
-  leftTree = new bTree(true)
-  leftTree.upNode = this
-  rightTree = new bTree(true)
-  rightTree.upNode = this
-  leftTree.node = leftNode
-  rightTree.node = rightNode
-  this.node[0].leftEdge = leftTree
-  this.node[0].rightEdge = rightTree
-  this.isLeaf = false
-}
-
-
-
-
-
+  var mid = Math.floor(this.node.length/2);
+  var leftNode = this.node.splice(0,mid);
+  var rightNode = this.node.splice(1, this.node.length);
+  leftTree = new bTree(true);
+  leftTree.upNode = this;
+  rightTree = new bTree(true);
+  rightTree.upNode = this;
+  leftTree.node = leftNode;
+  rightTree.node = rightNode;
+  this.node[0].leftEdge = leftTree;
+  this.node[0].rightEdge = rightTree;
+  this.isLeaf = false;
+};
