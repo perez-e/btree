9e9d7da8262871ee4b08cec7e4abbe952e761ab4
Eric Perez <ericricardoperez@gmail.com>
2014-04-16 11:29:52 -0700

    documentation for inserting into b-tree

--- btree.js
+++ btree.js
@@ -1,3 +1,4 @@
+// bTree object that represents a node with keys
 function bTree(isLeaf, isRoot){
   isRoot = isRoot || false;
   isLeaf = isLeaf || false;
@@ -8,26 +9,33 @@ function bTree(isLeaf, isRoot){
   this.m = 4;
 }
 
+// Key object that represents the keys in the b-tree node
+// value attr and edge attr connecting to other b-tree nodes
 function Key(value) {
   this.value = value;
   this.leftEdge = null;
   this.rightEdge = null;
 }
 
+// insert value into b-tree structure
 bTree.prototype.insert = function(num){
- 
+
+  // if current node is a leaf insert value acccrordingly 
   if ( this.isLeaf ) {
     this.insertIntoLeafNode(num);
   }
 
+  // if current node is not a leaf, transverse further until leaf is met 
   if ( !this.isLeaf ) {
     this.transverseTree(num);
   }
 
+  // if root node is saturated, split in half, and create new root from middle key
   if (this.node.length > this.m && this.isRoot){
     this.newRoot();
   }
 
+  // if current node is saturated, remove middle key and push to node above
   if (this.node.length > this.m && !this.isRoot ){
     this.overFlow();
   }
@@ -36,7 +44,11 @@ bTree.prototype.insert = function(num){
 
 bTree.prototype.overFlow = function(){
   var mid = Math.floor(this.node.length/2);
+
+  // extract middle key 
   var key = this.node[mid];
+
+  // create new b-tree strictres from left and right nodes
   var leftNode = this.node.splice(0,mid);
   var rightNode = this.node.splice(1, this.node.length);
   var leftTree = new bTree(true);
@@ -46,9 +58,11 @@ bTree.prototype.overFlow = function(){
   leftTree.node = leftNode;
   rightTree.node = rightNode;
 
+  // associate left and right edge of key to left and right b-tree structures
   key.leftEdge = leftTree;
   key.rightEdge = rightTree;
 
+  // insert key into the node above the current node
   if (key.value < this.upNode.node[0].value){
     this.upNode.node.unshift(key);
     this.upNode.node[1].leftEdge = rightTree;s
@@ -69,21 +83,23 @@ bTree.prototype.overFlow = function(){
       return;
     }
   }
-  
 
 };
 
 bTree.prototype.transverseTree = function(num){
+  // if num value is less than first key, transverse into left edge of first key
   if (num < this.node[0].value) {
     this.node[0].leftEdge.insert(num);
     return;
   }
 
+  // if num value is greater than last key, transverse into right edge of last key
   if (num >= this.node[this.node.length-1].value) {
     this.node[this.node.length-1].rightEdge.insert(num);
     return;
   }
 
+  // transverse into appropriate in between edge
   for (var i = 0; i < this.node.length - 1; i++ ){
     if (num >= this.node[i].value && num < this.node[i+1].value) {
       this.node[i].rightEdge.insert(num);
@@ -93,23 +109,28 @@ bTree.prototype.transverseTree = function(num){
 };
 
 bTree.prototype.insertIntoLeafNode = function(num){
+  // create key object
   var key = new Key(num);
 
+  // push into current node if empty
   if (this.node.length === 0){
     this.node.push(key);
     return ;
   }
 
+  // if key value is less than first key in node, insert at the beginning 
   if (key.value < this.node[0].value){
     this.node.unshift(key);
     return;
   }
 
+  // if key value is greater than last key in node, insert at the end
   if (key.value >= this.node[this.node.length-1].value) {
     this.node.push(key);
     return ;
   }
 
+  // insert key into appropriate position within current node
   for (var i = 0; i < this.node.length - 1; i++ ){
     if (key.value >= this.node[i].value && key.value < this.node[i+1].value){
       this.node.splice(i+1, 0, key);
@@ -121,14 +142,20 @@ bTree.prototype.insertIntoLeafNode = function(num){
 
 bTree.prototype.newRoot = function(){
   var mid = Math.floor(this.node.length/2);
+
+  // split right and left node in two
   var leftNode = this.node.splice(0,mid);
   var rightNode = this.node.splice(1, this.node.length);
+
+  // create new b-tree strictres from left and right nodes
   leftTree = new bTree(true);
   leftTree.upNode = this;
   rightTree = new bTree(true);
   rightTree.upNode = this;
   leftTree.node = leftNode;
   rightTree.node = rightNode;
+
+  // associate left and right edge of only key in root node to left and right b-tree structures
   this.node[0].leftEdge = leftTree;
   this.node[0].rightEdge = rightTree;
   this.isLeaf = false;
